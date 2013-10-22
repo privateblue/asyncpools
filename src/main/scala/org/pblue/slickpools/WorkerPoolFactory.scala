476c51c73d94cd5b20b5ea4b7c4b162863fb206d
Mikolaj Szabo <savingprivateblue@gmail.com>
2013-10-23 01:19:42 +0200

    handling exceptions due to missing config
    Signed-off-by: Mikolaj Szabo <savingprivateblue@gmail.com>

--- src/main/scala/org/pblue/slickpools/WorkerPoolFactory.scala
+++ src/main/scala/org/pblue/slickpools/WorkerPoolFactory.scala
@@ -5,11 +5,16 @@ import scala.concurrent.duration.Duration
 import akka.actor.ActorSystem
 import akka.util.Timeout
 
-import com.typesafe.config.{ Config, ConfigFactory }
+import com.typesafe.config.{ Config, ConfigFactory, ConfigException }
 
 trait WorkerPoolFactory extends PoolFactory {
 
-	private lazy val config = ConfigFactory.load
+	private lazy val config = 
+		try {
+			ConfigFactory.load
+		} catch {
+			case ce: ConfigException => throw new SlickpoolsException("Configuration failed to load", ce)
+		}
 
 	implicit val actorSystem = 
 		ActorSystem(
@@ -17,22 +22,75 @@ trait WorkerPoolFactory extends PoolFactory {
 			config)
 
 	def newConfiguredPool(name: String) = {
-		val poolConfig = config.getConfig(s"${configRoot}.${name}")
+		val poolConfig = 
+			try {
+				config.getConfig(s"${configRoot}.${name}")
+			} catch {
+				case ce: ConfigException => 
+					throw new SlickpoolsException(s"Unable to create pool $name, due to missing configuration", ce)
+			}
 		
-		val size = poolConfig.getInt("size")
+		val size = 
+			try {
+				poolConfig.getInt("size")
+			} catch {
+				case ce: ConfigException => 
+					throw new SlickpoolsException(s"Unable to create pool $name, due to missing or invalid size configuration", ce)
+			}
 		
-		val defaultTimeout = Timeout(Duration(poolConfig.getString("defaultTimeout")).toMillis)
+		val defaultTimeout = 
+			try {
+				Timeout(Duration(poolConfig.getString("defaultTimeout")).toMillis)
+			} catch {
+				case ce: ConfigException => 
+					throw new SlickpoolsException(s"Unable to create pool $name, due to missing or invalid defaultTimeout configuration", ce)
+			}
 
-		val maxNrOfRetries = poolConfig.getInt("maxNrOfRetries")
+		val maxNrOfRetries = 
+			try {
+				poolConfig.getInt("maxNrOfRetries")
+			} catch {
+				case ce: ConfigException => 
+					throw new SlickpoolsException(s"Unable to create pool $name, due to missing or invalid maxNrOfRetries configuration", ce)
+			}
 
-		val retryRange = Duration(poolConfig.getString("retryRange"))
+		val retryRange = 
+			try {
+				Duration(poolConfig.getString("retryRange"))
+			} catch {
+				case ce: ConfigException => 
+					throw new SlickpoolsException(s"Unable to create pool $name, due to missing or invalid retryRange configuration", ce)
+			}
+
+		val driver = 
+			try {
+				poolConfig.getString("driver")
+			} catch {
+				case ce: ConfigException => 
+					throw new SlickpoolsException(s"Unable to create pool $name, due to missing driver configuration", ce)
+			}
 
 		val ds = 
 			Datasource(
-				url = poolConfig.getString("url"),
-				user = poolConfig.getString("user"),
-				password = poolConfig.getString("password"),
-				driver = poolConfig.getString("driver"))
+				url = 
+					try {
+						poolConfig.getString("url")
+					} catch {
+						case ce: ConfigException => ""
+					},
+				user = 
+					try {
+						poolConfig.getString("user")
+					} catch {
+						case ce: ConfigException => ""
+					},
+				password = 
+					try {
+						poolConfig.getString("password")
+					} catch {
+						case ce: ConfigException => ""
+					},
+				driver = driver)
 
 		new WorkerPool(
 			name = name,
