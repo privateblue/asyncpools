45ea4f88b6f9c79912db57b1a625984856df33b9
Mikhail Mitrofanov <migmit@kinja.com>
2015-01-16 17:31:11 +0100

    Restarting connection worker if connection closes

--- src/main/scala/org/pblue/asyncpools/PoolableObjectFactory.scala
+++ src/main/scala/org/pblue/asyncpools/PoolableObjectFactory.scala
@@ -1,5 +1,7 @@
 package org.pblue.asyncpools
 
-trait PoolableObjectFactory[+T] {
-	def create: T
+trait PoolableObjectFactory[T] {
+        def create: T
+        def check(pooledObject: T): Unit // Throw an exception if pooled object is invalid
+        def postCheck(pooledObject: T): Unit = check(pooledObject) // Additional check after using the pooledObject to see if it's still valid
 }
