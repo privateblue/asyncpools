cfd6c715d9b93639a7bc8cbc20a6cfabb1b115d6
Ferenc Toth <feri@gawker.com>
2015-07-06 18:58:40 +0200

    doc

--- src/main/scala/org/pblue/asyncpools/Factory.scala
+++ src/main/scala/org/pblue/asyncpools/Factory.scala
@@ -1,6 +1,21 @@
 package org.pblue.asyncpools
 
+/**
+ * Is able to create [[Resource]]s and check the integrity of those.
+ * @tparam Resource The type of resources [[Worker]]s will operate on.
+ */
 trait Factory[Resource] {
+	/**
+	 * Create a new [[Resource instance]].
+	 * @return
+	 */
 	def create: Resource
+
+	/**
+	 * Will be called by [[Worker]]s if the [[Job]] they're handling results in an error.
+	 * If this method returns a [[Throwable]], it will be thrown, and the [[Worker]] will be restarted. as a result.
+	 * @param resource The [[Resource]] to check.
+	 * @return
+	 */
 	def check(resource: Resource): Option[Throwable]
 }
